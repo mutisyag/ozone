9bd4a912fd7102513f4f53f28b84eec12f8607ae
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2018-09-10 15:58:09 +0300

    Adding load_initial_fixtures mgmt command.

---
+++ ozone/core/management/commands/load_initial_fixtures.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    help = "Load initial data"
+
+    FIXTURES = ('regions',
+                'subregions',
+                'parties',
+                'meetings',
+                'treaties',
+                'annexes',
+                'groups',
+                'substances',
+                )
+
+    def handle(self, *args, **options):
+        for fixture in Command.FIXTURES:
+            call_command('loaddata', fixture)
