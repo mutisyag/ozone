80e7d00e46d51b2ad2ee1fb204cac781f6228ab8
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2018-09-11 16:46:47 +0300

    Making party views readonly

--- ozone/core/api/views.py
+++ ozone/core/api/views.py
@@ -5,18 +5,21 @@ from ..models import Region, Subregion, Party, Submission, Article7Questionnaire
 from ..serializers import RegionSerializer, SubregionSerializer, PartySerializer
 
 
-class RegionViewSet(viewsets.ModelViewSet):
+class ReadOnlyMixin:
+    def _allowed_methods(self):
+        return ['GET', 'OPTIONS']
+
+
+class RegionViewSet(ReadOnlyMixin, viewsets.ModelViewSet):
     queryset = Region.objects.all()
     serializer_class = RegionSerializer
 
 
-class SubregionViewSet(viewsets.ModelViewSet):
+class SubregionViewSet(ReadOnlyMixin, viewsets.ModelViewSet):
     queryset = Subregion.objects.all()
     serializer_class = SubregionSerializer
 
 
-class PartyViewSet(viewsets.ModelViewSet):
+class PartyViewSet(ReadOnlyMixin, viewsets.ModelViewSet):
     queryset = Party.objects.all()
     serializer_class = PartySerializer
-
-
