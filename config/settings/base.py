22abd25b8268e18c5afea373bcdec5626f5e3830
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2018-10-10 14:33:48 +0300

    cleanup

--- config/settings/base.py
+++ config/settings/base.py
@@ -155,15 +155,6 @@ MIDDLEWARE = [
 STATIC_ROOT = ROOT_DIR / 'static'
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
-# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-# STATICFILES_DIRS = [
-#     str(APPS_DIR / 'static'),
-# ]
-# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
-# STATICFILES_FINDERS = [
-#     'django.contrib.staticfiles.finders.FileSystemFinder',
-#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-# ]
 
 # MEDIA
 # ------------------------------------------------------------------------------
