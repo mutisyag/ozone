2639fd412e6c6d55d0266556c6b7a6b280e43a79
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2018-10-24 15:13:13 +0300

    initial selected data for questionaire fields

--- frontend/src/assets/questionnaire_fields.js
+++ frontend/src/assets/questionnaire_fields.js
@@ -7,7 +7,7 @@ export const intro_fields = [
 		label: '1.1. Did your country import CFCs, halons, carbon tetrachloride, methyl chloroform, HCFCs, HBFCs, bromochloromethane, or methyl bromide in the reporting year?',
 		type: 'radio',
 		name: 'has_imports',
-		selected: true,
+		selected: false,
 		options: [{text: 'Yes', value: true},{text: 'No', value: false}],
 		info:'If No, ignore data form 1 and go to question 1.2. If Yes, please complete data form 1. Please read Instruction I of the document carefully before filling in the form.',
 	},
@@ -15,7 +15,7 @@ export const intro_fields = [
 		label: '1.2. Did your country export or re-export CFCs, halons, carbon tetrachloride, methyl chloroform, HCFCs, HBFCs, bromochloromethane, or methyl bromide in the reporting year?',
 		type: 'radio',
 		name: 'has_exports',
-		selected: true,
+		selected: false,
 		options: [{text: 'Yes', value: true},{text: 'No', value: false}],
 		info: 'If No, ignore data form 2 and go to question 1.3. If Yes, please complete data form 2. Please read Instruction II of the document carefully before filling in the form.'
 	},
@@ -23,7 +23,7 @@ export const intro_fields = [
 		label: '1.3. Did your country produce CFCs, halons, carbon tetrachloride, methyl chloroform, HCFCs, HBFCs, bromochloromethane, or methyl bromide in the reporting year?',
 		type: 'radio',
 		name: 'has_produced',
-		selected: true,
+		selected: false,
 		options: [{text: 'Yes', value: true},{text: 'No', value: false}],
 		info: 'If No, ignore data form 3 and go to question 1.4. If Yes, please complete data form 3. Please read Instruction III of the document carefully before filling in the form.'
 	},	
@@ -31,7 +31,7 @@ export const intro_fields = [
 		label: '1.4. Did your country destroy any ODSs in the reporting year?',
 		type: 'radio',
 		name: 'has_destroyed',
-		selected: true,
+		selected: false,
 		options: [{text: 'Yes', value: true},{text: 'No', value: false}],
 		info: 'If No, ignore data form 4 and go to question 1.5. If Yes, please complete data form 4. Please read Instruction IV of the document carefully before filling in the form.'
 	},		
@@ -39,7 +39,7 @@ export const intro_fields = [
 		label: '1.5. Did your country import from or export or re-export to non-Parties in the reporting year?',
 		type: 'radio',
 		name: 'has_nonparty',
-		selected: null,
+		selected: false,
 		options: [{text: 'Yes', value: true},{text: 'No', value: false}],
 		info: 'If No, ignore data form 5. If Yes, please complete data form 5. Please read Instruction V of the document carefully, and, particularly, the definition of non-Parties before filling in the form.'
 	},
@@ -47,7 +47,7 @@ export const intro_fields = [
 		label: '1.6. DATA ON GENERATION OF HFC 23 FROM FACILITIES MANUFACTURING ANNEX C GROUP I OR ANNEX F SUBSTANCES ?',
 		type: 'radio',
 		name: 'has_emissions',
-		selected: true,
+		selected: false,
 		options: [{text: 'Yes', value: true},{text: 'No', value: false}],
 		info: 'If No, ignore data form 5. If Yes, please complete data form 5. Please read Instruction V of the document carefully, and, particularly, the definition of non-Parties before filling in the form.'
 	},		
