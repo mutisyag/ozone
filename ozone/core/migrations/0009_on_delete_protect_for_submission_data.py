4ef1eaf4b824d56da648a8347f1dab7cc85f6a0e
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2018-11-13 17:44:46 +0200

    Solving last-minute error :)

--- ozone/core/migrations/0009_on_delete_protect_for_submission_data.py
+++ ozone/core/migrations/0009_on_delete_protect_for_submission_data.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0008_submission_cloned_from.py'),
+        ('core', '0008_submission_cloned_from'),
     ]
 
     operations = [
