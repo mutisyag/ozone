fcba24deb038fe226b878131d828db6341c0496c
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2018-11-21 17:24:00 +0200

    spinner on api interceptors

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,57 @@
 <template>
-  <router-view></router-view>
+  <div>
+    
+    <div class="api-action-display" v-if="isLoading">
+      <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    </div>  
+
+    <router-view></router-view>
+  
+  </div>
 </template>
 
 <script>
+
 import Router from './router'
+import {api} from '@/api/api';
 export default {
   name: 'app',
   components: {routerview: Router},
+  data() {
+    return {
+      refCount: 0,
+      isLoading: false
+    }
+  },
+  methods: {
+    setLoading(isLoading) {
+      if (isLoading) {
+        this.refCount++;
+        this.isLoading = true;
+      } else if (this.refCount > 0) {
+        this.refCount--;
+        this.isLoading = (this.refCount > 0);
+      }
+    }
+  },
+  created(){
+    api.interceptors.request.use((config) => {
+      this.setLoading(true);
+      return config;
+    }, (error) => {
+      this.setLoading(false);
+      return Promise.reject(error);
+    });
+
+    api.interceptors.response.use((response) => {
+      this.setLoading(false);
+      return response;
+    }, (error) => {
+      this.setLoading(false);
+      return Promise.reject(error);
+    });
+  }
+
 }
 </script>
 
@@ -25,4 +70,75 @@ export default {
   @import '~bootstrap-vue/dist/bootstrap-vue.css';
   // Import Main styles for this application
   @import 'assets/scss/style';
+
+
+
+
+  .lds-ellipsis {
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+}
+.lds-ellipsis div {
+  position: absolute;
+  top: 27px;
+  width: 11px;
+  height: 11px;
+  border-radius: 50%;
+  background: red;
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.lds-ellipsis div:nth-child(1) {
+  left: 6px;
+  background: yellow;
+  animation: lds-ellipsis1 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(2) {
+  left: 6px;
+  background: blue;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(3) {
+  left: 26px;
+  background: green;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(4) {
+  left: 45px;
+  animation: lds-ellipsis3 0.6s infinite;
+}
+@keyframes lds-ellipsis1 {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes lds-ellipsis3 {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+@keyframes lds-ellipsis2 {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(19px, 0);
+  }
+}
+
+
+.api-action-display {
+  position: fixed;
+  top:50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  z-index: 1000;
+}
 </style>
