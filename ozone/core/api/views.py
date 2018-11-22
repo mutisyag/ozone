f7e3a5e0bbebb3f5cf7366e589dbe7e3ebc7b91a
Cătălin Jitea <catalinjitea@gmail.com>
2018-11-22 15:38:01 +0200

    Remove TransitionEvent model

--- ozone/core/api/views.py
+++ ozone/core/api/views.py
@@ -198,7 +198,7 @@ class SubmissionViewSet(viewsets.ModelViewSet):
     def call_transition(self, request, pk=None):
         if request.data.get('transition'):
             submission = Submission.objects.get(pk=pk)
-            submission.call_transition(request.data['transition'], request.user)
+            submission.call_transition(request.data['transition'])
             serializer = SubmissionSerializer(
                 submission,
                 many=False,
