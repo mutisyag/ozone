638f40b91a7d7407e889f60e38a791e11349d2ba
Andrei Melis <andrei.melis@eaudeweb.ro>
2018-11-23 18:01:09 +0200

    Fix migration conflict, refs #149

--- ozone/core/migrations/0015_obligation_form_type.py
+++ ozone/core/migrations/0015_obligation_form_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0013_auto_20181122_1738'),
+        ('core', '0014_adding_ordering_id_field'),
     ]
 
     operations = [
