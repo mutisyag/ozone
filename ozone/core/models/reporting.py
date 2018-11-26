97e7d2db7eb0d440b540b25a398b658ecac598f1
Cătălin Jitea <catalinjitea@gmail.com>
2018-11-26 15:53:10 +0200

    Remove unneeded try-except block

--- ozone/core/models/reporting.py
+++ ozone/core/models/reporting.py
@@ -387,17 +387,14 @@ class Submission(models.Model):
     def clone(self):
         is_cloneable, msg = self.check_cloning()
         if is_cloneable:
-            try:
-                clone = Submission.objects.create(
-                    party=self.party,
-                    reporting_period=self.reporting_period,
-                    obligation=self.obligation,
-                    cloned_from=self,
-                    created_by=self.created_by,
-                    last_edited_by=self.last_edited_by
-                )
-            except ValidationError as e:
-                raise e
+            clone = Submission.objects.create(
+                party=self.party,
+                reporting_period=self.reporting_period,
+                obligation=self.obligation,
+                cloned_from=self,
+                created_by=self.created_by,
+                last_edited_by=self.last_edited_by
+            )
         else:
             raise ValidationError(msg)
 
