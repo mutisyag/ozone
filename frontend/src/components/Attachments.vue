ab75f937c2caa305ebe404ee0eaaa915999bd22d
Dan Misailescu <dan.misailescu@gmail.com>
2018-11-27 19:15:48 +0200

    validated attachments file extensions

--- frontend/src/components/Attachments.vue
+++ frontend/src/components/Attachments.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<b-input-group style="margin-bottom:1rem;" prepend="Add file">
-			<b-form-file :multiple="true" accept="/*.*/" ref="fileinput" v-model="selectedFiles" @input="onSelectedFilesChanged" ></b-form-file>
+			<b-form-file :multiple="true" ref="fileinput" v-model="selectedFiles" @input="onSelectedFilesChanged" ></b-form-file>
 			<b-input-group-append>
 				<b-btn variant="primary" @click="saveAttachments" :disabled="isSaveDisabled"><i class="fa fa-floppy-o" aria-hidden="true"></i> Save changes</b-btn>
 			</b-input-group-append>
@@ -9,7 +9,7 @@
 		<b-list-group>			
 				<b-list-group-item style="font-size: 1.5rem" v-for="attachment in $store.state.form.tabs.attachments" :key="attachment.id">
 					<b-button variant="danger" class="pull-right" @click="deleteAttachment($event, attachment)">
-						<i class="fa fa-times" aria-hidden="true"></i>
+						<i class="fa fa-times" aria-hidden="true"></i> Delete
 					</b-button>
 					<a :href="attachment.url">
 						<i class="fa fa-file-zip-o fa-lg mt-4"></i>  
@@ -28,7 +28,7 @@ export default {
   data () {
     return {
 			selectedFiles: [],
-			isSaveDisabled: true
+			isSaveDisabled: true,			
     }
   },	
 	 methods: {
@@ -50,7 +50,10 @@ export default {
 		},	 
     async onSelectedFilesChanged() {
 			if(this.selectedFiles.length) {
-				await this.uploadFormAttachments(this.selectedFiles);
+				await this.uploadFormAttachments(this.selectedFiles.filter(file => file.type 
+																																						&& !file.name.endsWith('.exe')
+																																						&& !file.name.endsWith('.js')
+																																						&& !file.name.endsWith('.php')));
 				this.$refs.fileinput.reset();	
 				this.isSaveDisabled = false;
 			} 	
