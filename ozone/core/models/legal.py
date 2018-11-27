bf32a1e283f5e5a226b359ac4d2ca9e5c40657b5
Cătălin Jitea <catalinjitea@gmail.com>
2018-11-27 14:22:14 +0200

    Update validation logic for cloning and new submissions, refs #168

--- ozone/core/models/legal.py
+++ ozone/core/models/legal.py
@@ -47,15 +47,6 @@ class ReportingPeriod(models.Model):
     def __str__(self):
         return self.name
 
-    @classmethod
-    def current_period(cls):
-        current_date = datetime.now()
-        return (
-            cls.objects.filter(end_date__lt=current_date)
-            .order_by('-end_date')
-            .first()
-        )
-
     def clean(self):
         if self.end_date and self.start_date > self.end_date:
             raise ValidationError(
