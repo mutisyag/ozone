38a1c1eb3725a087a04ac01399677ad08fa4e419
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2018-11-28 13:10:16 +0200

    components cleanup and route based chunk loading

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <div>
-    
+  <div class="app">
     <div class="api-action-display" v-if="isLoading">
       <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
     </div>  
-
-    <router-view></router-view>
-  
+    <Alerts></Alerts>  
+    <AppHeader fixed>
+      <SidebarToggler class="d-lg-none" display="md" mobile />
+      <!-- <b-link class="navbar-brand" to="#"> -->
+        <!-- <img class="navbar-brand-full" width="89" height="25" alt="Logo"> -->
+        <!-- <img class="navbar-brand-minimized" width="30" height="30" alt="MobileLogo"> -->
+      <!-- </b-link> -->
+      <SidebarToggler class="d-md-down-none" display="lg" />
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item class="d-md-down-none">
+          <i class="icon-bell"></i>
+          <b-badge pill variant="danger">5</b-badge>
+        </b-nav-item>
+        <Header/>
+      </b-navbar-nav>
+      <!--<AsideToggler class="d-lg-none" mobile />-->
+    </AppHeader>
+    <div class="app-body">
+      <AppSidebar fixed>
+        <SidebarHeader/>
+        <SidebarForm/>
+        <SidebarNav :navItems="nav"></SidebarNav>
+        <SidebarFooter/>
+        <SidebarMinimizer/>
+      </AppSidebar>
+      <main class="main">
+        <Breadcrumb :list="list"/>
+        <div class="container-fluid">
+          <router-view></router-view>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
 
+import nav from '@/_nav'
+import Alerts from '@/views/Alerts'
+import { Header as AppHeader, SidebarToggler, Sidebar as AppSidebar, SidebarFooter, SidebarForm, SidebarHeader, SidebarMinimizer, SidebarNav, Aside as AppAside, Footer as TheFooter, Breadcrumb } from '@coreui/vue'
+import Header from '@/views/Header'
 import Router from './router'
 import {api} from '@/api/api';
 export default {
   name: 'app',
-  components: {routerview: Router},
+  components: {
+    AppHeader,
+    AppSidebar,
+    AppAside,
+    TheFooter,
+    Breadcrumb,
+    Header,
+    SidebarForm,
+    SidebarFooter,
+    SidebarToggler,
+    SidebarHeader,
+    SidebarNav,
+    SidebarMinimizer,
+    Alerts,
+    routerview: Router
+  },
+
   data() {
     return {
       refCount: 0,
-      isLoading: false
+      isLoading: false,
+      nav: nav.items
+
+    }
+  },
+
+  computed: {
+  name () {
+      return this.$route.name
+    },
+    list () {
+      return this.$route.matched.filter((route) => route.name || route.meta.label )
     }
   },
   methods: {
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,121 +1,77 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-// Containers
-const Base = () => import('@/views/Base')
+import App from './App'
+import Dashboard from '@/views/Dashboard'
 
-// Views
-const Dashboard = () => import('@/views/Dashboard')
-const Form = () => import('@/views/Form')
 
-const FormNotFound = () => import('@/views/FormNotFound')
-
-// Views - Components
-const Forms = () => import('@/views/base/Forms')
-
+const Form = () => import(/* webpackChunkName: "art7" */ '@/views/Form')
+const FormNotFound = () => import(/* webpackChunkName: "notFound" */ '@/views/FormNotFound')
 // Views - Pages
-const Page404 = () => import('@/views/pages/Page404')
-const Page500 = () => import('@/views/pages/Page500')
-const Login = () => import('@/views/pages/Login')
-const Register = () => import('@/views/pages/Register')
+const Page404 = () => import(/* webpackChunkName: "404" */ '@/views/pages/Page404')
+const Page500 = () => import(/* webpackChunkName: "500" */ '@/views/pages/Page500')
+const Login = () => import(/* webpackChunkName: "login" */ '@/views/pages/Login')
+const Register = () => import(/* webpackChunkName: "register" */ '@/views/pages/Register')
 
-// Users
-const Users = () => import('@/views/users/Users')
-const User = () => import('@/views/users/User')
 
 Vue.use(Router)
 
 const  routes = [
     {
       path: '/',
-      redirect: '/dashboard',
       name: 'Home',
-      component: Base,
-      children: [
-        {
-          path: 'dashboard',
-          name: 'Dashboard',
-          meta:{requiresAuth : true},
-          component: Dashboard
-        },
-        {
-          path: 'login',
-          name: 'Login',
-          component: Login
-        },
-        {
-          path: '/submission/art7',
-          name: 'art7',
-          meta:{requiresAuth : true},
-          component: Form,
-        },
-        {
-          path: '/submission/essencrit',
-          name: 'essencrit',
-          meta:{requiresAuth : true},
-          component: FormNotFound
-        },
-        {
-          path: '/submission/hatimp',
-          name: 'hatimp',
-          meta:{requiresAuth : true},
-          component: FormNotFound
-        },
-        {
-          path: '/submission/hatprod',
-          name: 'hatprod',
-          meta:{requiresAuth : true},
-          component: FormNotFound
-        },
-        {
-          path: '/submission/letter',
-          name: 'letter',
-          meta:{requiresAuth : true},
-          component: FormNotFound
-        },
-        {
-          path: 'users',
-          meta: { label: 'Users'},
-          component: {
-            render (c) { return c('router-view') }
-          },
-          children: [
-            {
-              path: '',
-              component: Users,
-            },
-            {
-              path: ':id',
-              meta: { label: 'User Details'},
-              name: 'User',
-              component: User,
-            },
-          ]
-        },
-        {
-          path: 'base',
-          redirect: '/base/forms',
-          name: 'Base',
-          component: {
-            render (c) { return c('router-view') }
-          },
-          children: [
-            {
-              path: 'forms',
-              name: 'Forms',
-              component: Forms
-            },
-          ]
-        },
-      ]
+      component: Dashboard,
+      redirect: '/dashboard'
+      // component: {
+      //   render (c) { return c('router-view') }
+      // },
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      meta:{requiresAuth : true},
+      component: Dashboard
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/submission/art7',
+      name: 'art7',
+      meta:{requiresAuth : true},
+      component: Form
+    },
+    {
+      path: '/submission/essencrit',
+      name: 'essencrit',
+      meta:{requiresAuth : true},
+      component: FormNotFound
+    },
+    {
+      path: '/submission/hatimp',
+      name: 'hatimp',
+      meta:{requiresAuth : true},
+      component: FormNotFound
+    },
+    {
+      path: '/submission/hatprod',
+      name: 'hatprod',
+      meta:{requiresAuth : true},
+      component: FormNotFound
+    },
+    {
+      path: '/submission/letter',
+      name: 'letter',
+      meta:{requiresAuth : true},
+      component: FormNotFound
     },
     {
       path: '/pages',
       redirect: '/pages/404',
       name: 'Pages',
-      component: {
-        render (c) { return c('router-view') }
-      },
+ 
       children: [
         {
           path: '404',
