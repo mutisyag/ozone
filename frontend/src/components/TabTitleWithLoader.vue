c89c5ba71d0e96ba7605f68676677b6e8ef7eb71
Dan Misailescu <dan.misailescu@gmail.com>
2018-12-07 13:55:18 +0200

    added titlewithloader.vue

---
+++ frontend/src/components/TabTitleWithLoader.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="tab-title">
+	{{tab.title}}
+	<div v-if="tab.status === 'saving'" class="spinner">
+		<div class="loader"></div>
+	</div>
+	<i v-if="tab.status === false" style="color: red;" class="fa fa-times-circle fa-lg"></i>
+	<i v-if="tab.status === true" style="color: green;" class="fa fa-check-circle fa-lg"></i>
+	<i v-if="tab.status === 'edited'" class="fa fa-edit fa-lg"></i>
+</div>
+</template>
+
+<script>
+
+export default {
+	props: {
+		tab: Object
+	}
+
+}
+</script>
