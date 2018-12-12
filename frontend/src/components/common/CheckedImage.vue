f982eefd3dd021ef351f4321b1380b495100ee92
Dan Misailescu <dan.misailescu@gmail.com>
2018-12-12 12:16:59 +0200

    added checked image

---
+++ frontend/src/components/common/CheckedImage.vue
@@ -0,0 +1,12 @@
+<template>
+	<i v-if="item" class="fa fa-check-square-o" aria-hidden="true"></i>
+	<i v-else class="fa fa-square-o" aria-hidden="true"></i>
+</template>
+
+<script>
+export default {
+	props: {
+		item: Object
+	}
+}
+</script>
