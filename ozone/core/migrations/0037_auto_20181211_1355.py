1cfc1eec2296fea3453a55eea0d740bb959e2c1d
Andrei Melis <andrei.melis@eaudeweb.ro>
2018-12-12 09:32:52 +0200

    Fixed migration conflict

--- ozone/core/migrations/0037_auto_20181211_1355.py
+++ ozone/core/migrations/0037_auto_20181211_1355.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0034_auto_20181211_0827'),
+        ('core', '0036_obligations'),
     ]
 
     operations = [
