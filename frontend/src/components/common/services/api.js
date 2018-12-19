dba552d4fcd088a0511a2e2e81c9955360f8d64b
Alexandru Chirila <chirila.s.alexandru@gmail.com>
2018-12-19 12:59:34 +0200

    Use the backend is_current filter. Refs. #332.

--- frontend/src/components/common/services/api.js
+++ frontend/src/components/common/services/api.js
@@ -89,6 +89,7 @@ const getSubmissions = (tableOptions) => {
 		params.obligation = tableOptions.filters.obligation
 		params.from_period = tableOptions.filters.period_start
 		params.to_period = tableOptions.filters.period_end
+		params.is_current = tableOptions.filters.isCurrent ? undefined : false
 	}
 	if (tableOptions.sorting) {
 		params.ordering = (tableOptions.sorting.sortDesc ? "-" : "") + tableOptions.sorting.sortBy
