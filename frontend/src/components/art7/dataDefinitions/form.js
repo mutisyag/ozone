7e5f38b726e145dac46423ef93f6f47dc32d3126
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2018-12-19 18:52:04 +0200

    flags generic and used in hat and art7

--- frontend/src/components/art7/dataDefinitions/form.js
+++ frontend/src/components/art7/dataDefinitions/form.js
@@ -822,42 +822,7 @@ const form = {
 				quantity_emitted: null
 			}
 		},
-<<<<<<< HEAD
 		flags: tab_flags
-=======
-		flags: {
-			name: 'flags',
-			hasAssideMenu: false,
-			endpoint_url: 'submission_flags_url',
-			status: null,
-			title: 'Flags',
-			titleHtml: '<b>Flags</b>',
-			tooltipHtml: '',
-			detailsHtml: 'Flags',
-			form_fields: flags,
-			get fields_order() {
-				return Object.keys(this.form_fields)
-			},
-			default_properties: {
-				flag_provisional: false,
-				flag_valid: null,
-				flag_superseded: false,
-				flag_checked_blanks: false,
-				flag_has_blanks: false,
-				flag_confirmed_blanks: false,
-				flag_has_reported_a1: false,
-				flag_has_reported_a2: false,
-				flag_has_reported_b1: false,
-				flag_has_reported_b2: false,
-				flag_has_reported_b3: false,
-				flag_has_reported_c1: false,
-				flag_has_reported_c2: false,
-				flag_has_reported_c3: false,
-				flag_has_reported_e: false,
-				flag_has_reported_f: false
-			}
-		}
->>>>>>> flags: save, prefill, moved to submission info
 	}
 }
 export default form
