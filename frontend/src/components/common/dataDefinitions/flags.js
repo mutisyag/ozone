fd309fd6d1a8b2aee0ae8ddb68b1a39087e1ceb3
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2018-12-19 18:43:16 +0200

    flags permissions and tooltip

--- frontend/src/components/common/dataDefinitions/flags.js
+++ frontend/src/components/common/dataDefinitions/flags.js
@@ -2,146 +2,82 @@ export default {
 	flag_provisional: {
 		selected: true,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true,
 		tooltip: 'test'
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_valid: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_superseded: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_checked_blanks: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_has_blanks: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_confirmed_blanks: {
 		selected: true,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_has_reported_a1: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_has_reported_a2: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_has_reported_b1: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_has_reported_b2: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_has_reported_b3: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_has_reported_c1: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_has_reported_c2: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_has_reported_c3: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_has_reported_e: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	},
 	flag_has_reported_f: {
 		selected: false,
 		type: 'checkbox',
-<<<<<<< HEAD
 		disabled: true
-=======
-		disabled: false
->>>>>>> flags: save, prefill, moved to submission info
 	}
 }
