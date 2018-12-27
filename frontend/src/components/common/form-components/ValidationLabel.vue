e2b2ec4fc2cd9b78c662f549cf61f596ddcd536f
Dan Misailescu <dan.misailescu@gmail.com>
2018-12-27 21:19:29 +0200

    #392 Extract repetitive code into Validation-Label component - added file

---
+++ frontend/src/components/common/form-components/ValidationLabel.vue
@@ -0,0 +1,42 @@
+<template>
+<span class="validation-wrapper">
+	<b-badge
+		pill
+		style="cursor:pointer"
+		variant="danger"
+		@click="openValidationCallback"
+		v-if="validation.length"
+		v-b-tooltip.hover
+		title="Click here to see the validation problems">
+			{{labels.invalid}}
+	</b-badge>
+	<b-badge v-else pill variant="success">{{labels.valid}}</b-badge>
+</span>
+</template>
+
+<script>
+import generalLabels from '@/components/common/dataDefinitions/labels'
+
+export default {
+	props: {
+		openValidationCallback: Function,
+		validation: Array
+	},
+
+	created() {
+		this.labels = generalLabels
+	},
+
+	components: { },
+
+	data() {
+		return {
+			labels: null
+		}
+	},
+
+	methods: {
+	}
+
+}
+</script>
