019bfe98d66c284ceebf919e4ba6615ee8de75cc
Alexandru Chirila <chirila.s.alexandru@gmail.com>
2019-01-07 16:04:36 +0200

    Also delete the submission info when deleting the Submission.
    Closes. #417.

--- ozone/core/models/reporting.py
+++ ozone/core/models/reporting.py
@@ -665,6 +665,8 @@ class Submission(models.Model):
             related_qs = getattr(self, related_data).all()
             if related_qs:
                 related_qs.delete()
+        if self.info:
+            self.info.delete()
 
         super().delete(*args, **kwargs)
 
