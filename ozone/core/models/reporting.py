6d758810068489c2ff8058b430a1ca7a78f990b0
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2019-01-08 14:24:11 +0200

    Removing unneeded delete.

--- ozone/core/models/reporting.py
+++ ozone/core/models/reporting.py
@@ -722,8 +722,6 @@ class Submission(models.Model):
             related_qs = getattr(self, related_data).all()
             if related_qs:
                 related_qs.delete()
-        if self.info:
-            self.info.delete()
 
         super().delete(*args, **kwargs)
 
