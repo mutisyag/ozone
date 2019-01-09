62dbb2884b181e6e009ae97b9e9b1384063b94a1
Travis CI <travis@travis-ci.org>
2019-01-09 19:30:53 +0200

    Add missing upstream in the push.

--- utility/push_from_travis.sh
+++ utility/push_from_travis.sh
@@ -14,7 +14,7 @@ commit_translations() {
 
 upload_files() {
   git remote add origin-translations https://${GH_TOKEN}@github.com/eaudeweb/ozone-translations.git > /dev/null 2>&1
-  git push --quiet
+  git push --quiet --set-upstream origin-translations
 }
 
 cd translations
