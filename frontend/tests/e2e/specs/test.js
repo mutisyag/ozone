08a75d5beef9063b76eba4ed99d65a9e40b2d7bb
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2019-01-10 15:32:50 +0200

    system call before test

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -1,7 +1,18 @@
+/* eslint-disable func-style */
+/* eslint-disable no-unused-vars */
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
 
+// or more concisely
+const sys = require('sys')
+const { exec } = require('child_process')
+
 module.exports = {
+	before: (browser) => {
+		console.log('Setting up...')
+		function puts(error, stdout, stderr) { sys.puts(stdout) }
+		exec('ls -la', puts)
+	},
 	'default e2e tests': browser => {
 		browser
 			.url(process.env.VUE_DEV_SERVER_URL)
