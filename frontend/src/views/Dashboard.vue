6bc192180b95fa82d1cf48ef5386a7c5c27b3aff
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2019-01-14 17:31:41 +0200

    BU_006 e2e test wip /refs #454

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -8,17 +8,17 @@
 			</div>
 			<small><span v-translate>Create a submission by specifying the obligation, the reporting period and the party name. All fields are mandatory.</span></small>
 			<div class="create-submission mt-2">
-				<b-input-group class="mb-2" :prepend="$gettext('Obligation')">
-				<multiselect trackBy="value" label="text" v-model="current.obligation" :options="obligations"></multiselect>
+				<b-input-group id="obligation_selector" class="mb-2" :prepend="$gettext('Obligation')">
+					<multiselect trackBy="value" label="text" v-model="current.obligation" :options="obligations"></multiselect>
 				</b-input-group>
 
-				<b-input-group class="mb-2" :prepend="$gettext('Period')">
-				<multiselect trackBy="value" label="text" customTemplateText="<i class='fa fa-clock-o fa-lg'></i>" customTemplate="is_reporting_open" v-model="current.reporting_period" :options="periods">
+				<b-input-group id="period_selector"  class="mb-2" :prepend="$gettext('Period')">
+					<multiselect trackBy="value" label="text" customTemplateText="<i class='fa fa-clock-o fa-lg'></i>" customTemplate="is_reporting_open" v-model="current.reporting_period" :options="periods">
 				</multiselect>
 				</b-input-group>
 
-				<b-input-group class="mb-2" :prepend="$gettext('Party')">
-				<multiselect trackBy="value" label="text" :disabled="Boolean(currentUser.party)" v-model="current.party" :options="parties"></multiselect>
+				<b-input-group id="party_selector" class="mb-2" :prepend="$gettext('Party')">
+					<multiselect trackBy="value" label="text" :disabled="Boolean(currentUser.party)" v-model="current.party" :options="parties"></multiselect>
 				</b-input-group>
 
 				<b-btn v-if="basicDataReady" :disabled="!(current.obligation && current.reporting_period && current.party)" variant="primary" @click="addSubmission"><span v-translate>Create</span></b-btn>
--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -4,28 +4,56 @@
 // http://nightwatchjs.org/guide#usage
 
 // or more concisely
+
 const util = require('util')
 const execSync = util.promisify(require('child_process').execSync)
+// const getSelectElementByContent = require('../custom-assertions/customSelectors')
 
 module.exports = {
 
-	before: () => {
+	beforeEach: () => {
 		console.log('running backend')
 		execSync('bash ../utility/setup_backend.sh', { env: process.env })
 		console.log('done running backend')
 	},
-	after: () => {
+	afterEach: () => {
 		console.log('running cleanup')
 		execSync('bash ../utility/cleanup_backend.sh', { env: process.env })
 		console.log('done running cleanup')
 	},
-	'default e2e tests': browser => {
+	BU_006: browser => {
 		browser
 			.url(process.env.VUE_DEV_SERVER_URL)
-			.waitForElementVisible('.app', 8000)
-			.assert.elementPresent('.app-header')
-			.assert.containsText('h3', 'ORS (Ozone online reporting system)')
-			.assert.elementCount('h3', 1)
+			// start login
+			.waitForElementVisible('#id_username', 20000)
+			.setValue('#id_username', 'party')
+			.setValue('#id_password', 'party')
+			.waitForElementVisible('button[type="submit"]', 10000)
+			.pause(1000)
+			.click('button[type="submit"]')
+			.waitForElementVisible('h3', 8000)
+			.assert.urlContains('/reporting/dashboard')
+		// end login
+		// start create submission
+			.waitForElementVisible('.create-submission', 10000)
+			.waitForElementVisible('#obligation_selector', 10000)
+			.waitForElementVisible('#obligation_selector .multiselect', 10000)
+			.click('#obligation_selector .multiselect')
+			.waitForElementVisible('#obligation_selector .multiselect__content-wrapper', 10000)
+			.useXpath()
+			.waitForElementVisible("//span[contains(text(),'Article 7')]/ancestor::div[contains(@id, 'obligation_selector')]", 5000)
+			.click('//div[@id="obligation_selector"]//ul//li//span//span[contains(text(),"Article 7")]')
+			.useCss()
+			.waitForElementVisible('#period_selector', 2000)
+			.waitForElementVisible('#period_selector .multiselect', 2000)
+			.click('#period_selector .multiselect')
+			.waitForElementVisible('#period_selector .multiselect__content-wrapper', 2000)
+			.useXpath()
+			.waitForElementVisible("//span[contains(text(),'2018')]/ancestor::div[contains(@id, 'period_selector')]", 5000)
+			.click('//div[@id="period_selector"]//ul//li//span//span[contains(text(),"2018")]')
+			.waitForElementVisible('//div[contains(@class,"create-submission")]//button', 5000)
+			.click('//div[contains(@class,"create-submission")]//button')
+		// end create submission
 			.end()
 	}
 }
