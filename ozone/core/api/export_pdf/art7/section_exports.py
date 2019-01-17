9da5def7c434c831c07a0f3b4cd511af1f0964f4
Irina Botez <irina.botez@eaudeweb.ro>
2019-01-17 13:45:22 +0200

    [refs #223] Refactoring exports section

--- ozone/core/api/export_pdf/art7/section_exports.py
+++ ozone/core/api/export_pdf/art7/section_exports.py
@@ -1,116 +1,52 @@
-from reportlab.platypus import Paragraph
-from reportlab.platypus import Table
-from reportlab.platypus import PageBreak
-
 from django.utils.translation import gettext_lazy as _
+from functools import partial
 
-from ..util import get_preship_or_polyols_q
-from ..util import get_decisions
-from ..util import get_quantities
-from ..util import get_quantity_cell
-from ..util import get_substance_label
-from ..util import p_c
-from ..util import p_l
+from reportlab.lib.units import mm
+from reportlab.platypus import PageBreak
+from reportlab.platypus import Paragraph
+from reportlab.platypus import Spacer
+from reportlab.platypus import Table
+
+from ..constants import TABLE_BLENDS_COMP_HEADER
+from ..constants import TABLE_BLENDS_COMP_STYLE
+from ..constants import TABLE_BLENDS_COMP_WIDTHS
 from ..util import page_title_section
 from ..util import STYLES
-from ..constants import TABLE_IMPORTS_EXPORTS_SUBS_WIDTHS as COL_WIDTHS
-from ..constants import TABLE_IMPORTS_EXPORTS_HEADER_STYLE
-from ..constants import TABLE_STYLES
-
-
-TABLE_IMPORTS_HEADER = (
-    (
-        p_c(_('Group')),
-        p_c(_('Substance')),
-        p_c(_('Country of destination of exports')),
-        p_c(_('Total Quantity Exported for All Uses')),
-        '',
-        p_c(_('Quantity of new substances exported as feedstock')),
-        p_c(_('Quantity of new substance exported for exempted essential,'
-              'critical, high-ambient-temperature or other uses')),
-        '',
-    ),
-    (
-        '',
-        '',
-        '',
-        p_c(_('New')),
-        p_c(_('Recovered and reclaimed')),
-        '',
-        p_c(_('Quantity')),
-        p_c(_('Decision / type of use or remark')),
-    ),
-)
-
-
-TABLE_ROW_EMPTY = (
-    (
-        _('No data.'),
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-    ),
-)
-
-
-TABLE_ROW_EMPTY_STYLE = (
-    ('SPAN', (0, 2), (-1, 2)),
-    ('VALIGN', (0, 2), (-1, 2), 'MIDDLE'),
-    ('ALIGN', (0, 2), (-1, 2), 'CENTER'),
-)
-
-
-def to_row_substance(obj):
-    substance = obj.substance
-
-    quantities = get_quantities(obj)
-    extra_q = get_preship_or_polyols_q(obj)
-    q_cell = get_quantity_cell(quantities, extra_q)
-
-    decisions = get_decisions(obj)
-    d_label = get_substance_label(decisions, type='decision', list_font_size=9)
-
-    dest_party = obj.destination_party.name if obj.destination_party else ""
-
-    return (
-        substance.group.group_id,
-        p_l(substance.name),
-        p_l(dest_party),
-        str(obj.quantity_total_new or ''),
-        str(obj.quantity_total_recovered or ''),
-        str(obj.quantity_feedstock or ''),
-        q_cell,
-        (d_label,)
-    )
+from .imp_exp_helper import big_table_row
+from .imp_exp_helper import component_row
+from .imp_exp_helper import table_from_data
 
 
 def mk_table_substances(submission):
-    # TODO: differentiate between blends and substances
-    exports = submission.article7exports.filter(blend_item__isnull=True)
-    return map(to_row_substance, exports)
-
+    exports = submission.article7exports.exclude(substance=None)
+    row = partial(big_table_row, isBlend=False)
+    return map(row, exports.filter(blend_item=None))
 
 def mk_table_blends(submission):
-    exports = submission.article7exports.filter(blend_item__isnull=False)
-    return map(to_row_substance, exports)
+    imports = submission.article7exports.filter(substance=None)
+    row = partial(big_table_row, isBlend=True)
+    blends = []
 
+    for blend_row in map(row, imports):
+        # Getting the blend object based on the id
+        blend = imports.filter(blend__blend_id=blend_row[1]).first()
+        row_comp = partial(component_row, blend=blend)
+        data = tuple(map(row_comp, blend.blend.components.all()))
 
-def table_from_data(data):
-    return Table(
-        TABLE_IMPORTS_HEADER + (data or TABLE_ROW_EMPTY),
-        colWidths=COL_WIDTHS,
-        style=(
-            TABLE_IMPORTS_EXPORTS_HEADER_STYLE + TABLE_STYLES + (
-                () if data else TABLE_ROW_EMPTY_STYLE
-            )
-        ),
-        repeatRows=2,  # repeat header on page break
-    )
+        blends.append(blend_row)
+        blends.append(
+            (
+                (Spacer(7, mm),
+                 Table(
+                     TABLE_BLENDS_COMP_HEADER + data,
+                     style=TABLE_BLENDS_COMP_STYLE,
+                     colWidths=TABLE_BLENDS_COMP_WIDTHS,
+                 ),
+                 Spacer(7, mm))
+                ,)
+        )
 
+    return blends
 
 def export_exports(submission):
     table_substances = tuple(mk_table_substances(submission))
@@ -118,10 +54,10 @@ def export_exports(submission):
 
     exports_page = (
         Paragraph(_('2.1 Substances'), STYLES['Heading2']),
-        table_from_data(table_substances),
+        table_from_data(table_substances, isBlend=False, type='export'),
         PageBreak(),
         Paragraph(_('2.2 Blends'), STYLES['Heading2']),
-        table_from_data(table_blends),
+        table_from_data(table_blends, isBlend=True, type='export'),
         PageBreak(),
     )
 
