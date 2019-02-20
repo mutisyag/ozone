177ddbc0d1050aee5bc1d77653e802a6fefec5d8
Cătălin Jitea <catalinjitea@gmail.com>
2019-02-20 15:43:25 +0200

    Fix bug when load fixtures directly in data migration

--- ozone/core/migrations/0036_obligations.py
+++ ozone/core/migrations/0036_obligations.py
@@ -1,60 +1,45 @@
 from django.db import migrations
+from django.core.serializers import base, python
 from django.core.management import call_command
-
-obligation_data = [
-  {
-    "model": "core.obligation",
-    "pk": 1,
-    "fields": {
-      "name": "Article 7",
-      "description": "Data forms 1-6",
-      "form_type": "art7",
-      "has_reporting_periods": True
-    }
-  },
-  {
-    "model": "core.obligation",
-    "pk": 2,
-    "fields": {
-      "name": "Essential and Critical uses (RAF)",
-      "description": "Reporting account format",
-      "form_type": "essencrit",
-      "has_reporting_periods": True
-    }
-  },
-  {
-    "model": "core.obligation",
-    "pk": 3,
-    "fields": {
-      "name": "HAT Imports and Production",
-      "description": "Data forms 7 and 8",
-      "form_type": "hat",
-      "has_reporting_periods": True
-    }
-  },
-  {
-    "model": "core.obligation",
-    "pk": 4,
-    "fields": {
-      "name": "Other",
-      "description": "Letters and narrative reports",
-      "form_type": "letter",
-      "has_reporting_periods": True
-    }
-  }
-]
 
 
 def refresh_obligations(apps, schema_editor):
+    """
+    loaddata command relies on django.core.serializers.python._get_model
+    function to get the corresponding model from a fixture, which will
+    return the most up-to-date version of a model. We need to monkey-patch it
+    so it gets the historical model.
+    See full explanation at https://stackoverflow.com/a/39743581
+    """
+
     Submission = apps.get_model('core', 'Submission')
     Submission.objects.filter(obligation_id__gte=3).delete()
     Obligation = apps.get_model('core', 'Obligation')
     Obligation.objects.filter(id__gte=3).delete()
-    # Populate obligations
-    for entry in obligation_data:
-        obj = Obligation(**entry['fields'])
-        obj.pk = entry['pk']
-        obj.save()
+
+    # Save the old _get_model() function
+    old_get_model = python._get_model
+
+    # Define new _get_model() function here, which utilizes the apps argument
+    # to get the historical version of a model. This piece of code is directly
+    # stolen from django.core.serializers.python._get_model, unchanged.
+    # However, here it has a different context, specifically, the apps variable.
+    def _get_model(model_identifier):
+        try:
+            return apps.get_model(model_identifier)
+        except (LookupError, TypeError):
+            raise base.DeserializationError(
+                "Invalid model identifier: '%s'" % model_identifier)
+
+    # Replace the _get_model() function on the module, so loaddata can utilize it.
+    python._get_model = _get_model
+
+    try:
+        # Call loaddata command
+        call_command('loaddata', 'obligations_old_v1')
+    finally:
+        # Restore old _get_model() function
+        python._get_model = old_get_model
 
 
 class Migration(migrations.Migration):
