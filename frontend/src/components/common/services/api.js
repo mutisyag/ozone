d08d206676f21e3ab21fc13e580122b17eae1f27
Alexandru Chirila <chirila.s.alexandru@gmail.com>
2019-03-12 09:23:58 +0200

    Also use a env variable for the API host.
    Default to the origin if the env var is not present.

--- frontend/src/components/common/services/api.js
+++ frontend/src/components/common/services/api.js
@@ -3,13 +3,14 @@ import tus from 'tus-js-client'
 
 const logRequests = process.env.NODE_ENV === 'development'
 
-// const BACKEND_HOST = 'localhost'
-// const BACKEND_PORT = 8000
-// let apiURL = `http://${BACKEND_HOST}:${BACKEND_PORT}/api/`;
-
 let apiURL = `${window.location.origin}/api`
 let apiBase = `${window.location.origin}`
 let filesURL = `${window.location.protocol}//${process.env.VUE_APP_TUSD_HOST}:${process.env.VUE_APP_TUSD_PORT}/files/`
+
+if (process.env.VUE_APP_API_HOST) {
+	apiURL = `${window.location.protocol}//${process.env.VUE_APP_API_HOST}/api`
+	apiBase = `${window.location.protocol}//${process.env.VUE_APP_API_HOST}`
+}
 
 let isTestSession = false
 if (process.env.NODE_ENV === 'development') {
