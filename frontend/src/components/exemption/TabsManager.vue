0cc90d7a51c55496f5acb6a0f445f0cf3fc31fdc
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2019-03-15 17:20:55 +0200

    use can_delete_data for exemptions /closes #647

--- frontend/src/components/exemption/TabsManager.vue
+++ frontend/src/components/exemption/TabsManager.vue
@@ -88,7 +88,7 @@
 				<b-btn @click="$refs.history_modal.show()" variant="outline-info">
 					<span v-translate>Versions</span>
 				</b-btn>
-				<b-btn @click="removeSubmission" v-if="$store.getters.can_edit_data"  variant="outline-danger">
+				<b-btn @click="removeSubmission" v-if="$store.state.current_submission.can_delete_data"  variant="outline-danger">
 					<span v-translate>Delete Submission</span>
 				</b-btn>
 			</b-button-group>
