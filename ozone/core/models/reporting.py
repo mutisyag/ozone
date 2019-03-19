304b127e53233b27c51ccd5a78d87b8e31b9d12f
Cătălin Jitea <catalinjitea@gmail.com>
2019-03-19 16:47:50 +0200

    Fix TypeError in clean method, refs #734

--- ozone/core/models/reporting.py
+++ ozone/core/models/reporting.py
@@ -180,7 +180,7 @@ class ReportingChannel(models.Model):
             queryset = ReportingChannel.objects.filter(**{field: True})
 
             if (
-                getattr(self, field, default=False) is True
+                getattr(self, field, False) is True
                 and queryset.count() > 0
                 and self not in queryset
             ):
