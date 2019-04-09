150bdcea28e3ef625dc69eafa322b2f18852b90d
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2019-04-09 16:27:00 +0300

    Implement feedback on error messages [refs #475]

--- frontend/src/components/common/dataDefinitions/alerts.js
+++ frontend/src/components/common/dataDefinitions/alerts.js
@@ -1,20 +1,20 @@
 const getAlerts = ($gettext) => {
   const alerts = {
     __all__: '',
-    clone_success: $gettext('A clone of this submission has been added.'),
-    cant_find_instructions: $gettext('Can\'t find instructions for current form'),
-    save_before_submit: $gettext('Please save before submitting.'),
-    questionaire_beforeSubmit: $gettext('Please complete the Questionnaire form before submitting.'),
+    clone_success: $gettext('A new version of this submission has been added.'),
+    cant_find_instructions: $gettext('No detailed instructions for the current form are available.'),
+    save_before_submit: $gettext('You have unsaved changes. Please save the submission first.'),
+    questionaire_beforeSubmit: $gettext('Please complete the Questionnaire before submitting.'),
     substance_already_exists: $gettext('Unable to add the following substances because they already exist'),
     select_country_before_adding_again: $gettext('select at least one country for each substance before adding.'),
     blend_already_exists: $gettext('Unable to add blend. Another blend with this name already exists.'),
     blend_not_added: $gettext('Unable to add the following blends because they already exist'),
     select_country_before_adding_again_blend: $gettext('select at least one country for each blend before adding.'),
     blend_created: $gettext('Custom blend added successfully.'),
-    field_already_exists: $gettext('Unable to add fields for the following countries because they already exist'),
+    field_already_exists: $gettext('Unable to add rows for the following countries because they already exist'),
     save_failed: $gettext('Unable to save submission. Please try again.'),
-    save_before_submitting: $gettext('Please save the document before submitting.'),
-    new_version_created: $gettext('A clone of this submission has been added.')
+    save_before_submitting: $gettext('You have unsaved changes. Please save the submission first.'),
+    new_version_created: $gettext('A new version of this submission has been added.')
   }
   return alerts
 }
