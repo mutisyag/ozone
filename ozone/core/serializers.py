424e5721e2a223ef7be73d19dd688c58748abedd
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2019-04-15 16:55:27 +0300

    Adding critical uses flag to serializer

--- ozone/core/serializers.py
+++ ozone/core/serializers.py
@@ -315,6 +315,7 @@ class SubstanceSerializer(serializers.ModelSerializer):
             'id', 'name', 'description', 'sort_order',
             'odp', 'formula', 'number_of_isomers', 'min_odp', 'max_odp',
             'is_qps', 'is_contained_in_polyols', 'is_captured',
+            'has_critical_uses',
         )
 
 
