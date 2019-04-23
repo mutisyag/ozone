bdd269053757e7251098bb8358f5531abc035e8e
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2019-04-23 18:17:08 +0300

    qps first in modal /refs #894

--- frontend/src/components/art7/dataDefinitions/modalFieldsOrder.js
+++ frontend/src/components/art7/dataDefinitions/modalFieldsOrder.js
@@ -1,8 +1,8 @@
 const order = [
-  'quantity_laboratory_analytical_uses',
-  'decision_laboratory_analytical_uses',
   'quantity_quarantine_pre_shipment',
   'decision_quarantine_pre_shipment',
+  'quantity_laboratory_analytical_uses',
+  'decision_laboratory_analytical_uses',
   'quantity_essential_uses',
   'decision_essential_uses',
   'quantity_critical_uses',
