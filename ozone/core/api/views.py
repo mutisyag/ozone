3deb4804bda0af7c1ed5b1206813f28bd03f8bd3
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2019-05-08 18:22:02 +0300

    Doing query only once.

--- ozone/core/api/views.py
+++ ozone/core/api/views.py
@@ -265,10 +265,13 @@ class GetNonPartiesViewSet(ReadOnlyMixin, views.APIView):
         groups = Group.objects.all()
         all_non_parties = {}
 
+        period_pk = ReportingPeriod.objects.get(name=period_name).pk \
+            if period_name else None
+
         for group in groups:
             queryset = Article7NonPartyTrade.get_non_parties(
                 group.pk,
-                ReportingPeriod.objects.get(name=period_name).pk if period_name else None,
+                period_pk
             )
             non_parties_per_group = {
                 id: True for id in queryset.values_list('id', flat=True)
