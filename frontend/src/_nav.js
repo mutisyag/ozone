0f82a9d4d4ce0ac2467babb1c5a69ca0f4106a03
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2019-05-13 15:39:13 +0300

    icons in sidebar

--- frontend/src/_nav.js
+++ frontend/src/_nav.js
@@ -1,17 +1,20 @@
 const getNav = ($gettext) => [{
   name: $gettext('Dashboard'),
   url: '/dashboard',
-  icon: 'icon-calculator'
+  icon: 'icon-screen-desktop'
 },
 {
   name: $gettext('Substances'),
-  url: '/lookup-tables/controlled-substances'
+  url: '/lookup-tables/controlled-substances',
+  icon: 'icon-chemistry'
 }, {
   name: $gettext('Blends'),
-  url: '/lookup-tables/blends'
+  url: '/lookup-tables/blends',
+  icon: 'icon-layers'
 }, {
   name: $gettext('Parties'),
-  url: '/lookup-tables/parties'
+  url: '/lookup-tables/parties',
+  icon: 'icon-people'
 },
 {
   name: $gettext('Production'),
