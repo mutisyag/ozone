83a779357973154178ab7a9eff1d556e23d48061
Cătălin Jitea <catalinjitea@gmail.com>
2019-05-16 20:05:32 +0300

    A little refactoring when preparing data for exporting prod/cons, refs #881

--- ozone/core/api/export_pdf/art7/others.py
+++ ozone/core/api/export_pdf/art7/others.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from ozone.core.models import (
     ProdCons,
     Group,
@@ -34,14 +32,16 @@ def get_prodcons_data(submission):
         reporting_period=period
     )
     if submission.submitted_at:
-        date_reported = submission.submitted_at
+        date_reported = submission.submitted_at.strftime("%d-%b-%Y")
+    elif submission.info.date:
+        date_reported = submission.info.date.strftime("%d-%b-%Y")
     else:
-        date_reported = submission.info.date
+        date_reported = ''
     data['party'] = {
         'name': party.name,
         'population': history.population,
         'party_type': history.party_type.abbr,
-        'date_reported': date_reported.strftime("%d-%b-%Y")
+        'date_reported': date_reported
     }
 
     data['headers'] = {
@@ -59,50 +59,53 @@ def get_prodcons_data(submission):
         'baseline_cons': 0,
     }
     for group in all_groups:
-        if agg.get(group):
-            prodcons = agg[group]
+        prodcons = agg.get(group)
+        if prodcons and prodcons.calculated_production:
             actual_prod = prodcons.calculated_production
-            if prodcons.baseline_prod:
-                baseline_prod = prodcons.baseline_prod
-                if actual_prod <= 0 or baseline_prod == 0:
-                    chng_prod = -100
-                else:
-                    chng_prod = round(
-                        -100 + actual_prod / baseline_prod*100,
-                        ProdCons.get_decimals(period, group, party)
-                    )
+            total['actual_prod'] += actual_prod
+        else:
+            if group in to_report_groups:
+                actual_prod = 'NR'
             else:
-                baseline_prod = 'NR'
-                chng_prod = -100
+                actual_prod = '-'
+
+        per_capita_cons = 0
+        if prodcons and prodcons.calculated_consumption:
             actual_cons = prodcons.calculated_consumption
-            if prodcons.baseline_cons:
-                baseline_cons = prodcons.baseline_cons
-                if actual_cons <= 0 or baseline_cons == 0:
-                    chng_cons = -100
-                else:
-                    chng_cons = round(
-                        -100 + actual_cons / baseline_cons*100,
-                        ProdCons.get_decimals(period, group, party)
-                    )
-            else:
-                baseline_cons = 'NR'
-                chng_cons = -100
+            total['actual_cons'] += actual_cons
             per_capita_cons = round(
                 actual_cons / history.population,
                 4
             )
-            total['actual_prod'] += actual_prod
-            total['baseline_prod'] += baseline_prod if baseline_prod != 'NR' else 0
-            total['actual_cons'] += actual_cons
-            total['baseline_cons'] += baseline_cons if baseline_cons != 'NR' else 0
         else:
             if group in to_report_groups:
-                actual_prod = chng_prod = 'NR'
-                actual_cons = chng_cons = per_capita_cons = 'NR'
+                actual_cons = 'NR'
             else:
-                actual_prod = chng_prod = '-'
-                actual_cons = chng_cons = per_capita_cons = '-'
-            baseline_prod = baseline_cons = 'NR'
+                actual_cons = '-'
+
+        chng_prod = -100
+        if prodcons and prodcons.baseline_prod:
+            baseline_prod = prodcons.baseline_prod
+            if not isinstance(actual_prod, str) and actual_prod > 0 and baseline_prod != 0:
+                chng_prod = round(
+                    -100 + actual_prod / baseline_prod*100,
+                    ProdCons.get_decimals(period, group, party)
+                )
+            total['baseline_prod'] += baseline_prod
+        else:
+            baseline_prod = 'NR'
+
+        chng_cons = -100
+        if prodcons and prodcons.baseline_cons:
+            baseline_cons = prodcons.baseline_cons
+            if not isinstance(actual_cons, str) and actual_cons > 0 and baseline_cons != 0:
+                chng_cons = round(
+                    -100 + actual_cons / baseline_cons*100,
+                    ProdCons.get_decimals(period, group, party)
+                )
+            total['baseline_cons'] += baseline_cons
+        else:
+            baseline_cons = 'NR'
 
         limit_prod = Limit.objects.filter(
             party=party,
@@ -120,6 +123,7 @@ def get_prodcons_data(submission):
         ).first()
         if limit_cons:
             limit_cons = limit_cons.limit
+
         data['data'][group.group_id] = {
             'description': group.description,
             'actual_prod': actual_prod,
@@ -132,9 +136,10 @@ def get_prodcons_data(submission):
             'limit_cons': limit_cons,
             'per_capita_cons': per_capita_cons
         }
+
     total['actual_prod'] = round(total['actual_prod'], 2)
     total['baseline_prod'] = round(total['baseline_prod'], 2)
-    if total['actual_prod'] <= 0:
+    if total['actual_prod'] <= 0 or total['baseline_prod'] == 0:
         total['chng_prod'] = - 100
     else:
         total['chng_prod'] = round(
@@ -143,7 +148,7 @@ def get_prodcons_data(submission):
         )
     total['actual_cons'] = round(total['actual_cons'], 2)
     total['baseline_cons'] = round(total['baseline_cons'], 2)
-    if total['actual_cons'] <= 0:
+    if total['actual_cons'] <= 0 or total['baseline_cons'] == 0:
         total['chng_cons'] = - 100
     else:
         total['chng_cons'] = round(
