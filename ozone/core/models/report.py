771aa4e4e04ac50f2cdecd0e6a30bdd51a5e4bc2
Cătălin Jitea <catalinjitea@gmail.com>
2019-05-27 13:22:17 +0300

    Add required params for the available report endpoints, refs #997

--- ozone/core/models/report.py
+++ ozone/core/models/report.py
@@ -13,5 +13,19 @@ class Reports(enum.Enum):
     PRODCONS = 'prodcons'
 
     @staticmethod
+    def args(has_party_param=False, has_period_param=False):
+        return {
+            'has_party_param': has_party_param,
+            'has_period_param': has_period_param
+        }
+
+    @staticmethod
+    def prodcons_args():
+        return Reports.args(has_party_param=True, has_period_param=True)
+
+    @staticmethod
     def items():
-        return {e.name: e.value for e in Reports}
+        return [
+            {**{'name': e.value}, **getattr(Reports, e.value + "_args")()}
+            for e in Reports
+        ]
