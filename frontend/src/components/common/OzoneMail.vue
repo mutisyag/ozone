f84a883406df6ccc27c3196cec98708bc19e2791
Mihai Macaneata <themacaneata@gmail.com>
2019-05-28 11:56:41 +0300

    wip

---
+++ frontend/src/components/common/OzoneMail.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <b-tabs>
+            <b-tab :title="$gettext('Send a message')">
+                <b-row>
+                    <b-col cols="8">
+                        <textarea v-model="currentMessage" class="form-control"></textarea>
+                    </b-col>
+                    <b-col></b-col>
+                </b-row>
+            </b-tab>
+            <b-tab v-if="history" :title="$gettext('Conversation history')">
+            </b-tab>
+        </b-tabs>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      currentMessage: '',
+      templates: null,
+      history: null
+    }
+  }
+}
+</script>
