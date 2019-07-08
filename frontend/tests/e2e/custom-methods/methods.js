096c919229a1f7751f135e6e5cb0c0553f0efc28
Miu Razvan <miu.razvan28@gmail.com>
2019-07-08 13:16:51 +0300

    Fixed tests

--- frontend/tests/e2e/custom-methods/methods.js
+++ frontend/tests/e2e/custom-methods/methods.js
@@ -1,8 +1,16 @@
-const logMessage = (browser, message) => {
+const logMessage = (browser, message, header = false) => {
   browser.perform((done) => {
-    console.log('==================================================================')
+    if (header) {
+      console.log('===============')
+    } else {
+      console.log('---------------')
+    }
     console.log(message)
-    console.log('==================================================================')
+    if (header) {
+      console.log('===============')
+    } else {
+      console.log('---------------')
+    }
     done()
   })
 }
@@ -64,7 +72,7 @@ const login = (browser, username, password, mouse = false) => {
 }
 
 const logout = (browser) => {
-  logMessage(browser, 'Log out')
+  logMessage(browser, 'Log out', false)
   browser.useCss()
     .waitForElementVisible('header.app-header .navbar-nav a.dropdown-toggle', 5000)
     .moveToElement('header.app-header .navbar-nav a.dropdown-toggle', undefined, undefined)
@@ -343,9 +351,10 @@ const openDashboard = (browser) => {
 }
 
 const openGeneralInstructions = (browser) => {
+  logMessage(browser, 'Opening General Instructions')
   browser.useXpath()
-    .waitForElementVisible("//button[contains(@class, 'btn-outline-info')]", 10000)
-    .click("//button[contains(@class, 'btn-outline-info')]")
+    .waitForElementVisible("//button/i[contains(@class, 'fa-info')]", 10000)
+    .click("//button/i[contains(@class, 'fa-info')]")
     .pause(500)
     .execute('window.scrollTo(0,0)')
     .pause(500)
@@ -733,20 +742,20 @@ const addValues = (browser, table, tab, row, row_values, modal_values) => {
       .pause(500)
 
     browser
-      .waitForElementVisible(`#${tab} .modal-body`, 5000)
+      .waitForElementVisible(`#${tab} #edit_modal .modal-body`, 5000)
       .pause(500)
     /* Add values in modal */
     for (const field_id of Object.keys(modal_values)) {
       browser
-        .click(`#${tab} .modal-body #${field_id}`)
+        .click(`#${tab} #edit_modal .modal-body #${field_id}`)
         .pause(200)
-        .clearValue(`#${tab} .modal-body #${field_id}`)
-        .setValue(`#${tab} .modal-body #${field_id}`, modal_values[field_id])
+        .clearValue(`#${tab} #edit_modal .modal-body #${field_id}`)
+        .setValue(`#${tab} #edit_modal .modal-body #${field_id}`, modal_values[field_id])
     }
     /* Close modal */
     browser
       .pause(500)
-      .click(`#${tab} .modal-dialog button span[data-msgid="Close"]`)
+      .click(`#${tab} #edit_modal .modal-dialog button span[data-msgid="Close"]`)
       .pause(500)
   }
   /* Show app-footer */
@@ -796,19 +805,19 @@ const rowIsEmpty = (browser, table, tab, row, row_values, modal_values, start_co
       .click(`#${tab} #${table} tbody tr:nth-child(${row}) td .row-controls span:not(.table-btn)`)
   })
   browser
-    .waitForElementVisible(`#${tab} .modal-body`, 5000)
+    .waitForElementVisible(`#${tab} #edit_modal .modal-body`, 5000)
     .pause(500)
   /* Check if modal inputs are empty */
   for (const field_id of Object.keys(modal_values)) {
     browser
-      .getValue(`#${tab} .modal-body #${field_id}`, (data) => {
+      .getValue(`#${tab} #edit_modal .modal-body #${field_id}`, (data) => {
         browser.assert.equal(data.value, '')
       })
   }
   /* Close modal */
   browser
     .pause(500)
-    .click(`#${tab} .modal-dialog .close`)
+    .click(`#${tab} #edit_modal .modal-dialog .close`)
     .pause(500)
     .execute(`document.querySelector("#${tab} #${table} tbody tr:nth-child(${row})").classList.remove("hovered")`, () => {})
     /* Show app-footer */
@@ -836,6 +845,7 @@ const uploadeFile = (browser, filename, filepath) => {
 }
 
 module.exports = {
+  logMessage,
   showMouse,
   login,
   logout,
