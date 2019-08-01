72ca14d489c474980b8a3971872e30c87fc09023
Cătălin Jitea <catalinjitea@gmail.com>
2019-08-01 13:29:37 +0300

    Add model for licensing systems, refs #1151

--- ozone/core/models/country_profile.py
+++ ozone/core/models/country_profile.py
@@ -3,6 +3,27 @@ from django.db import models
 from . import Party, Submission
 
 
+class LicensingSystem(models.Model):
+    party = models.ForeignKey(
+        Party, related_name='licensing_systems', on_delete=models.PROTECT
+    )
+    has_ods = models.BooleanField(default=False)
+    date_reported_ods = models.DateField(null=True)
+    has_hfc = models.BooleanField(default=False)
+    date_reported_hfc = models.DateField(null=True)
+    remarks = models.CharField(max_length=9999, blank=True)
+
+    submission = models.ForeignKey(
+        Submission,
+        related_name='licensing_systems',
+        on_delete=models.PROTECT,
+        null=True
+    )
+
+    class Meta:
+        db_table = "licensing_system"
+
+
 class FocalPoint(models.Model):
     party = models.ForeignKey(
         Party, related_name='focal_points', on_delete=models.PROTECT
