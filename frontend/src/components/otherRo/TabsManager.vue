d0fbb7366c65cf0bebbada53630cdd19cadc55fc
mihai-macaneata <themacaneata@gmail.com>
2019-09-13 14:24:39 +0300

    sidebar visiblity functionality

--- frontend/src/components/otherRo/TabsManager.vue
+++ frontend/src/components/otherRo/TabsManager.vue
@@ -174,12 +174,6 @@ export default {
     selectedTab() {
       const { form } = this.$store.state
       const tab = form.tabs[form.formDetails.tabsDisplay[this.tabIndex]]
-      const body = document.querySelector('body')
-      if (tab.hasAssideMenu && !this.$store.getters.isReadOnly) {
-        body.classList.add('aside-menu-lg-show')
-      } else {
-        body.classList.remove('aside-menu-lg-show')
-      }
       return tab
     },
     formTabs() {
