5a838783a11168eaa20b367da43a638f7122c695
mihai-macaneata <themacaneata@gmail.com>
2019-09-13 15:53:55 +0300

    lint

--- frontend/src/components/raf/services/tableRowConstructorService.js
+++ frontend/src/components/raf/services/tableRowConstructorService.js
@@ -73,7 +73,7 @@ export default {
         selected: substance || null
       },
       quantity_exempted: {
-        type: 'nonInput',        
+        type: 'nonInput',
         selected: null,
         exemptionValue
       },
