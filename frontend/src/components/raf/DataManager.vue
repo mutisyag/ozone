9aed1bd01f73bfd8d010a07d7f7147a79573b3b6
Andrei Melis <andrei.melis@eaudeweb.ro>
2019-11-19 17:38:18 +0200

    Fix RAF form not loading, fixes #1287

--- frontend/src/components/raf/DataManager.vue
+++ frontend/src/components/raf/DataManager.vue
@@ -7,12 +7,62 @@
 <script>
 import tabsManager from '@/components/raf/TabsManager'
 import dataManagerMixin from '@/components/common/mixins/DataManagerMixin'
+import {
+  isObject,
+  getPropertyValue
+} from '@/components/common/services/utilsService'
 
 export default {
   name: 'DataManager',
   components: {
     tabsmanager: tabsManager
   },
-  mixins: [dataManagerMixin]
+  mixins: [dataManagerMixin],
+
+  computed: {
+     initialDataReady() {
+      this.$store.commit('setPreventLeaveConfirm', false)
+      if (!this.form) {
+        return false
+      }
+      for (const propertyPath of this.form.formDetails.dataNeeded) {
+        const propValue = getPropertyValue(this.$store.state, propertyPath)
+        if (propertyPath === 'submissionDefaultValues.submission_format') {
+          // eslint-disable-next-line no-continue
+          continue
+        }
+      }
+
+      const { dataNeeded } = this.form.formDetails
+      Object.values(this.form.tabs).forEach(tab => {
+        if (isObject(tab.form_fields)) {
+          for (const formFieldPropName in tab.form_fields) {
+            const formField = tab.form_fields[formFieldPropName]
+            if (formField.optionsStatePropertyPath) {
+              for (const propertyPath of dataNeeded) {
+                if (formField.optionsStatePropertyPath === propertyPath) {
+                  const propValue = getPropertyValue(this.$store.state, propertyPath)
+                  formField.options = propValue
+                  break
+                }
+              }
+            }
+            if (formField.selectedPropertyPath) {
+              for (const propertyPath of dataNeeded) {
+                if (formField.selectedPropertyPath === propertyPath) {
+                  const propValue = getPropertyValue(this.$store.state, propertyPath)
+                  if (formField.selected !== propValue) {
+                    formField.selected = propValue
+                  }
+                  break
+                }
+              }
+            }
+          }
+        }
+      })
+      return this.prefilled
+    }
+  }
 }
 </script>
