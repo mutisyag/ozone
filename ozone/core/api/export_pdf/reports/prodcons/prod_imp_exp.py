794af65e4d4ea3b0cec1d62e434e80a0fd1da544
Alex Morega <alex@grep.ro>
2019-11-20 15:29:59 +0200

    Render headings for parties

--- ozone/core/api/export_pdf/reports/prodcons/prod_imp_exp.py
+++ ozone/core/api/export_pdf/reports/prodcons/prod_imp_exp.py
@@ -1,10 +1,18 @@
 from reportlab.platypus import PageBreak
 from reportlab.platypus import Paragraph
 
+from ozone.core.models import Obligation
+from ozone.core.models import ObligationTypes
+from ozone.core.models import Party
+from ozone.core.models import PartyHistory
+from ozone.core.models import Submission
+
 from ozone.core.api.export_pdf.util import h2_style
 from ozone.core.api.export_pdf.util import DOUBLE_HEADER_TABLE_STYLES
 from ozone.core.api.export_pdf.util import col_widths
 from ozone.core.api.export_pdf.util import TableBuilder
+from ozone.core.api.export_pdf.util import get_date_of_reporting_str
+from ozone.core.api.export_pdf.util import format_decimal
 
 from . import data
 from . import render
@@ -38,6 +46,40 @@ class ProdImpExpTable:
             self.period.name, "Base", "% Chng",
         ])
 
+    def party_heading(self, party, history, date_reported):
+        badges = []
+
+        if history.is_article5:
+            badges.append("A5")
+        else:
+            badges.append("Non-A5")
+
+        if history.is_ceit:
+            badges.append("CEIT")
+
+        if history.is_eu_member:
+            badges.append("EU")
+
+        return (f"{party.name}  (Date Reported: {date_reported}) - {' '.join(badges)}  "
+                f"(Population: {format_decimal(history.population)})")
+
+    def render_parties(self):
+        parties = Party.get_main_parties()
+        art7 = Obligation.objects.get(_obligation_type=ObligationTypes.ART7.value)
+        submission_map = Submission.latest_submitted_for_parties(art7, self.period, parties)
+        histories = PartyHistory.objects.filter(reporting_period=self.period)
+        history_map = {h.party: h for h in histories}
+
+        for party in parties:
+            submission = submission_map.get(party)
+            if not submission:
+                continue
+
+            history = history_map[party]
+            date_reported = get_date_of_reporting_str(submission)
+            heading = self.party_heading(party, history, date_reported)
+            self.builder.add_heading(heading)
+
     def done(self):
         return self.builder.done()
 
@@ -59,6 +101,7 @@ def get_prod_imp_exp_flowables(periods):
         yield from groups_description
 
         table = ProdImpExpTable(period)
+        table.render_parties()
         yield table.done()
 
         yield PageBreak()
