f7acbca2ae719d8488f535484b9b63aa214f329a
Alex Morega <alex@grep.ro>
2019-11-20 17:20:41 +0200

    Render easy values

--- ozone/core/api/export_pdf/reports/prodcons/prod_imp_exp.py
+++ ozone/core/api/export_pdf/reports/prodcons/prod_imp_exp.py
@@ -1,10 +1,14 @@
+from collections import defaultdict
+
 from reportlab.platypus import PageBreak
 from reportlab.platypus import Paragraph
 
+from ozone.core.models import Group
 from ozone.core.models import Obligation
 from ozone.core.models import ObligationTypes
 from ozone.core.models import Party
 from ozone.core.models import PartyHistory
+from ozone.core.models import ProdCons
 from ozone.core.models import Submission
 
 from ozone.core.api.export_pdf.util import h2_style
@@ -23,28 +27,57 @@ TABLE_CUSTOM_STYLES = (
     ('SPAN', (1, 0), (3, 0)),  # production
     ('SPAN', (4, 0), (6, 0)),  # imports
     ('SPAN', (7, 0), (9, 0)),  # exports
-    # ('ALIGN', (1, 2), (-1, -1), 'RIGHT'),
+    ('ALIGN', (1, 2), (-1, -1), 'RIGHT'),
 )
 
 class ProdImpExpTable:
 
     def __init__(self, period):
         self.period = period
+
+        self.all_groups = list(Group.objects.all())
+        histories = PartyHistory.objects.filter(reporting_period=period)
+        self.history_map = {h.party: h for h in histories}
+        self.prodcons_map = self.get_prodcons_map()
+        self.parties = Party.get_main_parties()
+        art7 = Obligation.objects.get(_obligation_type=ObligationTypes.ART7.value)
+        self.submission_map = Submission.latest_submitted_for_parties(art7, self.period, self.parties)
+
+        self.format = data.ValueFormatter()
+        self.builder = self.begin_table()
+
+    def get_prodcons_map(self):
+        rv = defaultdict(defaultdict)
+
+        for row in ProdCons.objects.filter(reporting_period=self.period).iterator():
+            if row.group in rv[row.party]:
+                raise RuntimeError(f"duplicate wtf: {row.pk} {row}")
+            rv[row.party][row.group] = row
+
+        return dict(rv)
+
+    def get_baselines(self, party):
+        history = self.history_map[party]
+        assert not history.is_article5
+        return (0, 0)
+
+    def begin_table(self):
         styles = list(DOUBLE_HEADER_TABLE_STYLES + TABLE_CUSTOM_STYLES)
         column_widths = col_widths([6, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5])
-        self.builder = TableBuilder(styles, column_widths)
-        self.builder.add_row([
+        builder = TableBuilder(styles, column_widths)
+        builder.add_row([
             "",
             "PRODUCTION", "", "",
             "IMPORTS", "", "",
             "EXPORTS", "", "",
         ])
-        self.builder.add_row([
+        builder.add_row([
             "",
             self.period.name, "Base", "% Chng",
             self.period.name, "Base", "% Chng",
             self.period.name, "Base", "% Chng",
         ])
+        return builder
 
     def party_heading(self, party, history, date_reported):
         badges = []
@@ -63,22 +96,45 @@ class ProdImpExpTable:
         return (f"{party.name}  (Date Reported: {date_reported}) - {' '.join(badges)}  "
                 f"(Population: {format_decimal(history.population)})")
 
-    def render_parties(self):
-        parties = Party.get_main_parties()
-        art7 = Obligation.objects.get(_obligation_type=ObligationTypes.ART7.value)
-        submission_map = Submission.latest_submitted_for_parties(art7, self.period, parties)
-        histories = PartyHistory.objects.filter(reporting_period=self.period)
-        history_map = {h.party: h for h in histories}
+    def format_comparison(self, value, baseline):
+        return [
+            self.format.prodcons(value),
+            self.format.baseline(baseline, value),
+            self.format.change(value, baseline),
+        ]
 
-        for party in parties:
-            submission = submission_map.get(party)
-            if not submission:
+    def render_party(self, party):
+        submission = self.submission_map.get(party)
+        if not submission:
+            return
+
+        history = self.history_map[party]
+        date_reported = get_date_of_reporting_str(submission)
+        heading = self.party_heading(party, history, date_reported)
+        self.builder.add_heading(heading)
+
+        for group in self.all_groups:
+            prodcons = self.prodcons_map[party].get(group)
+            if not prodcons:
                 continue
 
-            history = history_map[party]
-            date_reported = get_date_of_reporting_str(submission)
-            heading = self.party_heading(party, history, date_reported)
-            self.builder.add_heading(heading)
+            value_prod = prodcons.calculated_production
+            value_import = prodcons.import_new
+            value_export = prodcons.export_new
+
+            baseline_prod = prodcons.baseline_prod
+            (baseline_import, baseline_export) = self.get_baselines(party)
+
+            row = [group.name]
+            row += self.format_comparison(value_prod, baseline_prod)
+            row += self.format_comparison(value_import, baseline_import)
+            row += self.format_comparison(value_export, baseline_export)
+
+            self.builder.add_row(row)
+
+    def render_parties(self):
+        for party in self.parties:
+            self.render_party(party)
 
     def done(self):
         return self.builder.done()
