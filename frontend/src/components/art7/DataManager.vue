29e1dfd079c2841c77a2e0bda54093e567821037
Miu Razvan <miu.razvan28@gmail.com>
2019-12-05 11:47:48 +0200

    Eslint warnings fixed

--- frontend/src/components/art7/DataManager.vue
+++ frontend/src/components/art7/DataManager.vue
@@ -19,49 +19,47 @@ export default {
   },
   mixins: [dataManagerMixin],
   initialDataReady() {
-      this.$store.commit('setPreventLeaveConfirm', false)
-      if (!this.form) {
-        return false
+    this.$store.commit('setPreventLeaveConfirm', false)
+    if (!this.form) {
+      return false
+    }
+    for (const propertyPath of this.form.formDetails.dataNeeded) {
+      const propValue = getPropertyValue(this.$store.state, propertyPath)
+      if (propertyPath === 'submissionDefaultValues.submission_format') {
+        // eslint-disable-next-line no-continue
+        continue
       }
-      for (const propertyPath of this.form.formDetails.dataNeeded) {
-        const propValue = getPropertyValue(this.$store.state, propertyPath)
-        if (propertyPath === 'submissionDefaultValues.submission_format') {
-          // eslint-disable-next-line no-continue
-          continue
-        }
-        if (!propValue) return false
-      }
-
-      const { dataNeeded } = this.form.formDetails
-      Object.values(this.form.tabs).forEach(tab => {
-        if (isObject(tab.form_fields)) {
-          for (const formFieldPropName in tab.form_fields) {
-            const formField = tab.form_fields[formFieldPropName]
-            if (formField.optionsStatePropertyPath) {
-              for (const propertyPath of dataNeeded) {
-                if (formField.optionsStatePropertyPath === propertyPath) {
-                  const propValue = getPropertyValue(this.$store.state, propertyPath)
-                  formField.options = propValue
-                  break
-                }
+      if (!propValue) return false
+    }
+    const { dataNeeded } = this.form.formDetails
+    Object.values(this.form.tabs).forEach(tab => {
+      if (isObject(tab.form_fields)) {
+        for (const formFieldPropName in tab.form_fields) {
+          const formField = tab.form_fields[formFieldPropName]
+          if (formField.optionsStatePropertyPath) {
+            for (const propertyPath of dataNeeded) {
+              if (formField.optionsStatePropertyPath === propertyPath) {
+                const propValue = getPropertyValue(this.$store.state, propertyPath)
+                formField.options = propValue
+                break
               }
             }
-            if (formField.selectedPropertyPath) {
-              for (const propertyPath of dataNeeded) {
-                if (formField.selectedPropertyPath === propertyPath) {
-                  const propValue = getPropertyValue(this.$store.state, propertyPath)
-                  if (formField.selected !== propValue) {
-                    formField.selected = propValue
-                  }
-                  break
+          }
+          if (formField.selectedPropertyPath) {
+            for (const propertyPath of dataNeeded) {
+              if (formField.selectedPropertyPath === propertyPath) {
+                const propValue = getPropertyValue(this.$store.state, propertyPath)
+                if (formField.selected !== propValue) {
+                  formField.selected = propValue
                 }
+                break
               }
             }
           }
         }
-      })
-
-      return this.prefilled
-    },
+      }
+    })
+    return this.prefilled
+  }
 }
 </script>
