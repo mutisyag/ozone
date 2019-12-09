6d45e8a6e0cb59ddbfae758bd9ee4ed4e9de615d
Alex Morega <alex@grep.ro>
2019-12-09 18:37:12 +0200

    Use group name instead of group_id

--- ozone/core/api/export_pdf/reports/impexp.py
+++ ozone/core/api/export_pdf/reports/impexp.py
@@ -121,7 +121,7 @@ class RecoveredImportExportTable:
 
             if isinstance(substance, Substance):
                 substance_txt = str(substance)
-                group_txt = self.group_map[substance.group_id].group_id
+                group_txt = self.group_map[substance.group_id].name
             else:
                 substance_txt = f"{substance} ({substance.composition})"
                 group_txt = ""
