2890a6b322e196cfa10104c2e3ffd9a2981dc5e7
Miu Razvan <miu.razvan28@gmail.com>
2019-12-12 13:33:24 +0200

    Fixed flag and language not showing when refresh in lookup pages

--- frontend/src/views/lookupTables/Parties.vue
+++ frontend/src/views/lookupTables/Parties.vue
@@ -237,6 +237,7 @@ export default {
     }
   },
   created() {
+    this.$store.dispatch('getDashboardParties')
     const body = document.querySelector('body')
     if (body.classList.contains('aside-menu-lg-show')) {
       document.querySelector('body').classList.remove('aside-menu-lg-show')
